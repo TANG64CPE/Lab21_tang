595c4a87a7c073ac3e9379df8dbb6a0f0e465614
ThankZ <natsuphat_th@cmu.ac.th>
2022-02-01 18:01:52 +0700

    non-completed

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,22 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
+	Rect out;
+	double Anser,A1,A2;
+	if(R1.y > (R2.y-R2.h) || (R1.x+R1.w) > R2.x){
+		A1 = ( R1.x + R1.w ) - R1.x;
+		A2 = ( R2.y + R2.h ) - R2.y;
+		Anser = A1*A2;
+	}else{
+		Anser = 0;
+	}
+	out;
+}
 
-
-
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};
+	overlap(R1,R2);
+	return 0;
 }
